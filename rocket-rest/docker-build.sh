a1f9f0712acd19dcf0480ea06f741a8093632302
michael <michael.howard.h@gmail.com>
2023-09-02 22:42:12 +0200

    Rocket Example

---
+++ rocket-rest/docker-build.sh
@@ -0,0 +1,3 @@
+docker build --tag docker-rocket-rest-image .
+docker tag docker-rust-image:latest docker-rocket-rest-image:0.0.1
+docker images
---
+++ rocket-rest/docker-run.sh
@@ -0,0 +1,1 @@
+docker run -d -p 3001:8000 docker-rocket-rest-image
